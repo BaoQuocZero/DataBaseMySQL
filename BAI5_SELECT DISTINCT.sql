3c273f2bd7d6659dd4e923ea2f0eb0af5f51882e
Quoc Bao Code <baoquoczero@gmail.com>
2023-10-14 20:50:08 +0700

    up goc

---
+++ BAI5_SELECT DISTINCT.sql
@@ -0,0 +1,24 @@
+﻿-- SELECT DISTINCT là câu lệnh láy ra dữ liệu khác nhau mà không bị trùng lập
+
+-- Viết câu lệnh in ra tên quốc gia [Country] khác nhau
+-- Từ bảng khách hàng [Customers] mà không bị trùng lập
+SELECT DISTINCT [Country]
+FROM [dbo].[Customers];
+
+--Viết câu lệnh in ra mã bưu điện [PostalCode] khác nhau
+--Từ bảng nhà cung cấp [Suppliers] mà không bị trùng lập
+SELECT DISTINCT [PostalCode]
+FROM [dbo].[Suppliers];
+
+--Viết câu lệnh in họ nhân viên lastName
+--và cách gọi danh hiệu lịch sự TitleOfCourtesy của nhân viên từ bảng Employyees
+
+SELECT DISTINCT [LastName], [TitleOfCourtesy]
+FROM [dbo].[Employees]
+
+SELECT DISTINCT [TitleOfCourtesy]
+FROM [dbo].[Employees]
+
+--Viết câu lệnh in mã đơn vị vận chuyển ShipVia kacs nhau của các đơn hàng Orders
+SELECT DISTINCT [ShipVia]
+FROM [dbo].[Orders];
