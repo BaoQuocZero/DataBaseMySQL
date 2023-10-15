db745131e3af8b13bc8bc63054fa4a16c6a620e1
Quoc Bao Code <baoquoczero@gmail.com>
2023-10-15 19:22:03 +0700

    up

---
+++ BAI4_SELECT.sql
@@ -0,0 +1,15 @@
+﻿--Câu lệnh lấy tên tất cả sản phẩm
+SELECT [ProductName]
+FROM [dbo].[Products];
+
+--Câu lệnh lấy tên tất cả sản phẩm, giá bán mỗi đơn vị, số lượng sản phẩm mỗi đơn vị
+SELECT [ProductName], [UnitPrice], [QuantityPerUnit]
+FROM [dbo].[Products];
+
+--Câu lệnh lấy tên khách hàng và quốc gia khách đó
+SELECT [CompanyName], [Country]
+FROM [dbo].[Customers];
+
+--Câu lệnh lấy tên và số đt cua tất cả nhà cung cấp hàng
+SELECT [CompanyName], [Phone]
+FROM [dbo].[Suppliers];
