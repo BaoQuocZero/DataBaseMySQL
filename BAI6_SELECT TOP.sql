1b2420f36bff0635d1ff606774ac5fa682371aa4
Quoc Bao Code <baoquoczero@gmail.com>
2023-10-17 17:21:07 +0700

    up

---
+++ BAI6_SELECT TOP.sql
@@ -0,0 +1,15 @@
+﻿--Lấy ra 5 dòng đầu tiên trong customers
+SELECT TOP 5 *
+FROM Customers;
+
+--Lấy ra 30% thông tin nhân viên
+SELECT TOP 30 PERCENT *
+FROM Employees;
+
+--Lấy ra các đơn hàng với mã khách hàng không trùng, chỉ lấy 5 dòng đầu
+SELECT DISTINCT TOP 5 [CustomerID]
+FROM [dbo].[Orders];
+
+--Lấy ra các sản phẩm có mã thể loại không trùng lặp và chỉ lấy 3 dòng đầu
+SELECT DISTINCT TOP 3 [ProductID]
+FROM [dbo].[Products];
